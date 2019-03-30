cb6900b9ef415aa672e78bd8b11b867271d30ecf
SpaceƇowboy <38109642+rafa1um@users.noreply.github.com>
2019-03-30 15:17:49 -0300

    Add files via upload

---
+++ tcp/client/tcp_client.py
@@ -0,0 +1,19 @@
+import socket
+
+server = socket.socket()    # cria um socket
+tcp_ip = 'localhost'    # ip para autenticacao
+tcp_port = 33000    # porta usada na trasnferencia
+BUFSIZ = 1024   # quantidade de bytes que será enviado por vez
+server.connect((tcp_ip, tcp_port))  # faz a conexao com ip e porta
+f = open('indice.jpeg', 'rb')   # abre arquivo que sera enviado
+l = f.read(BUFSIZ)  # le os primeiros BUFSIZ bytes do arquivo (1024 bytes)
+
+while(l):   # enquanto nao for final do arquivo, continua o loop
+        print("Enviando...")
+        server.sendall(l)   # envia os bytes lidos para o servidor
+        l = f.read(BUFSIZ)  # le os proximos BUFSIZ bytes do arquivo
+f.close()   # fecha o arquivo
+print("Enviado!")
+server.shutdown(socket.SHUT_RDWR)
+# envia uma notificacao de desligament para o servidor
+server.close()  # fecha a conexao
---
+++ tcp/server/tcp_server.py
@@ -0,0 +1,31 @@
+import socket
+
+
+server = socket.socket()
+
+tcp_ip = 'localhost'
+tcp_port = 33000
+BUFSIZ = 1024
+server.bind((tcp_ip, tcp_port))
+f = open('indice.jpeg', 'wb')
+# cria um arquivo que sera escrito com os dados do arquivo recebido
+server.listen(5)    # servidor aguarda uma conexao
+
+while True:     # loop infinito
+    conn, addr = server.accept()    # estabelece conexao com o client
+    print(addr, "Conectou-se")
+    print("Recebendo...")
+    l = conn.recv(BUFSIZ)
+    # recebe os primeiros BUFSIZ bytes enviados pelo client
+    while(l):
+        print("Recebendo arquivo...")
+        f.write(l)
+        l = conn.recv(BUFSIZ)
+        # recebe os proximos BUFSIZ bytes enviados pelo client
+    f.close()   # fecha o arquivo
+    print("Recebido!")
+    finConn = "Conexão finalizada!"
+    conn.sendall(finConn.encode('utf-8'))
+    # avisa que a conexao finalizou (nao ta funcionando)
+    conn.close()    # fecha a conexao
+    break   # quebra o loop
