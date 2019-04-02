de7b2cfbe308f985f3a8581f12c3828f207c79dd
rafa1um <rafael.rxp1@gmail.com>
2019-04-02 08:47:45 -0300

    sei lá

--- tcp/client/tcp_client.py
+++ tcp/client/tcp_client.py
@@ -5,13 +5,8 @@ tcp_ip = 'localhost'    # ip para autenticacao
 tcp_port = 33000    # porta usada na trasnferencia
 BUFSIZ = 200   # quantidade de bytes que será enviado por vez
 server.connect((tcp_ip, tcp_port))  # faz a conexao com ip e porta
-<<<<<<< HEAD
-
-f = open('indice.jpeg', 'rb')   # abre arquivo que sera enviado
-=======
 file = input()
 f = open(file, 'rb')   # abre arquivo que sera enviado
->>>>>>> 78c0c81ea7e08c2bcd4c58b360dac7b502d9623e
 l = f.read(BUFSIZ)  # le os primeiros BUFSIZ bytes do arquivo (1024 bytes)
 
 while(l):   # enquanto nao for final do arquivo, continua o loop
